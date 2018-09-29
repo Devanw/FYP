717664cdb0c403a48b80de74346504582f124231
Devanw <devan9e03@ymail.com>
2018-09-30 01:14:10 +0800

    Created Geometry

---
+++ app/src/main/java/com/example/user/fyp/Math/Geometry1.java
@@ -0,0 +1,15 @@
+package com.example.user.fyp.Math;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.user.fyp.R;
+
+public class Geometry1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geometry1);
+    }
+}
