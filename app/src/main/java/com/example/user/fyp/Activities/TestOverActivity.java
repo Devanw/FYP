40ad675626cdb98f233dfc267df9115ecde5137d
Devanw <devanewaldo@gmail.com>
2018-10-18 17:30:03 +0800

    Fixed onbackpressed

--- app/src/main/java/com/example/user/fyp/Activities/TestOverActivity.java
+++ app/src/main/java/com/example/user/fyp/Activities/TestOverActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.fyp.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,4 +13,11 @@ public class TestOverActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_over);
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(getApplicationContext(), MathTopicActivity.class);
+        startActivity(intent);
+   }
 }
