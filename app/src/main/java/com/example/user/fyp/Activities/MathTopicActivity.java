035dc35ab278e0d722214d8d648802b3fe5c46d5
Devanw <devanewaldo@gmail.com>
2018-10-18 16:18:35 +0800

    Refactored code

--- app/src/main/java/com/example/user/fyp/Activities/MathTopicActivity.java
+++ app/src/main/java/com/example/user/fyp/Activities/MathTopicActivity.java
@@ -23,87 +23,31 @@ public class MathTopicActivity extends AppCompatActivity {
 
     private static final String TAG = "MathTopic";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_math_topic);
-        final Button buttonSummation1 = findViewById(R.id.mathTopicActivity_bSummation1);
-        buttonSummation1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: start");
-                Intent intent = new Intent(getApplicationContext(), Summation1.class);
-                Log.d(TAG, "onClick: before intent");
-                startActivity(intent);
-            }
-        });
+    Button buttonCounting;
+    Button buttonCounting2;
+    Button buttonSummation1;
+    Button buttonSubtraction1;
+    Button buttonGeo;
+    Button buttonSCounting;
+    Button buttonSGeo;
+    Spinner spinnerStudyOrTest;
+    ArrayAdapter<CharSequence> adapter;
 
-        final Button buttonCounting = findViewById(R.id.mathTopicActivity_bCounting1);
-        buttonCounting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: start");
-                Intent intent = new Intent(getApplicationContext(), CountingTopic.class);
-                Log.d(TAG, "onClick: before intent");
-                startActivity(intent);
-            }
-        });
+    protected void establish(){
+        buttonCounting = findViewById(R.id.mathTopicActivity_bCounting1);
+        buttonCounting2 = findViewById(R.id.mathTopicActivity_bCounting2);
+        buttonSummation1 = findViewById(R.id.mathTopicActivity_bSummation1);
+        buttonSubtraction1 = findViewById(R.id.mathTopicActivity_bSubtraction1);
+        buttonGeo = findViewById(R.id.mathTopicActivity_bGeometry1);
+        buttonSCounting = findViewById(R.id.mathTopicActivity_bSCounting);
+        buttonSGeo = findViewById(R.id.mathTopicActivity_bSGeometry);
+        spinnerStudyOrTest =  findViewById(R.id.mathTopicActivity_spinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.spinnerFill, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerStudyOrTest.setAdapter(adapter);
+    }
 
-        final Button buttonCounting2 = findViewById(R.id.mathTopicActivity_bCounting2);
-        buttonCounting2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: start");
-                Intent intent = new Intent(getApplicationContext(), CountingTopic2.class);
-                Log.d(TAG, "onClick: before intent");
-                startActivity(intent);
-            }
-        });
-
-        final Button buttonSubtraction1 = findViewById(R.id.mathTopicActivity_bSubtraction1);
-        buttonSubtraction1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: start");
-                Intent intent = new Intent(getApplicationContext(), Subtraction1.class);
-                Log.d(TAG, "onClick: before intent");
-                startActivity(intent);
-            }
-        });
-
-        final Button buttonGeo = findViewById(R.id.mathTopicActivity_bGeometry1);
-        buttonGeo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: start");
-                Intent intent = new Intent(getApplicationContext(), Geometry1.class);
-                Log.d(TAG, "onClick: before intent");
-                startActivity(intent);
-            }
-        });
-
-        final Button buttonSCounting = findViewById(R.id.mathTopicActivity_bSCounting);
-        buttonSCounting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: start");
-                Intent intent = new Intent(getApplicationContext(), StudyCounting.class);
-                Log.d(TAG, "onClick: before intent");
-                startActivity(intent);
-            }
-        });
-
-        final Button buttonSGeo = findViewById(R.id.mathTopicActivity_bSGeometry);
-        buttonSGeo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: start");
-                Intent intent = new Intent(getApplicationContext(), StudyGeometry.class);
-                Log.d(TAG, "onClick: before intent");
-                startActivity(intent);
-            }
-        });
-
+    protected void setTest(){
         buttonCounting.setVisibility(View.VISIBLE);
         buttonCounting.setClickable(true);
         buttonCounting2.setVisibility(View.VISIBLE);
@@ -118,45 +62,111 @@ public class MathTopicActivity extends AppCompatActivity {
         buttonSCounting.setClickable(false);
         buttonSGeo.setVisibility(View.INVISIBLE);
         buttonSGeo.setClickable(false);
+    }
 
-        Spinner spinnerStudyOrTest =  findViewById(R.id.mathTopicActivity_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinnerFill, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerStudyOrTest.setAdapter(adapter);
+    protected void setStudy(){
+        buttonCounting.setVisibility(View.INVISIBLE);
+        buttonCounting.setClickable(false);
+        buttonCounting2.setVisibility(View.INVISIBLE);
+        buttonCounting2.setClickable(false);
+        buttonGeo.setVisibility(View.INVISIBLE);
+        buttonGeo.setClickable(false);
+        buttonSummation1.setVisibility(View.INVISIBLE);
+        buttonSummation1.setClickable(false);
+        buttonSubtraction1.setVisibility(View.INVISIBLE);
+        buttonSubtraction1.setClickable(false);
+        buttonSCounting.setVisibility(View.VISIBLE);
+        buttonSCounting.setClickable(true);
+        buttonSGeo.setVisibility(View.VISIBLE);
+        buttonSGeo.setClickable(true);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math_topic);
+
+        establish();
+
+        buttonSummation1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: start");
+                Intent intent = new Intent(getApplicationContext(), Summation1.class);
+                Log.d(TAG, "onClick: before intent");
+                startActivity(intent);
+            }
+        });
+
+        buttonCounting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: start");
+                Intent intent = new Intent(getApplicationContext(), CountingTopic.class);
+                Log.d(TAG, "onClick: before intent");
+                startActivity(intent);
+            }
+        });
+
+
+        buttonCounting2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: start");
+                Intent intent = new Intent(getApplicationContext(), CountingTopic2.class);
+                Log.d(TAG, "onClick: before intent");
+                startActivity(intent);
+            }
+        });
+
+        buttonSubtraction1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: start");
+                Intent intent = new Intent(getApplicationContext(), Subtraction1.class);
+                Log.d(TAG, "onClick: before intent");
+                startActivity(intent);
+            }
+        });
+
+        buttonGeo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: start");
+                Intent intent = new Intent(getApplicationContext(), Geometry1.class);
+                Log.d(TAG, "onClick: before intent");
+                startActivity(intent);
+            }
+        });
+
+        buttonSCounting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: start");
+                Intent intent = new Intent(getApplicationContext(), StudyCounting.class);
+                Log.d(TAG, "onClick: before intent");
+                startActivity(intent);
+            }
+        });
+
+        buttonSGeo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: start");
+                Intent intent = new Intent(getApplicationContext(), StudyGeometry.class);
+                Log.d(TAG, "onClick: before intent");
+                startActivity(intent);
+            }
+        });
+
         spinnerStudyOrTest.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if (position == 0){
-                    buttonCounting.setVisibility(View.INVISIBLE);
-                    buttonCounting.setClickable(false);
-                    buttonCounting2.setVisibility(View.INVISIBLE);
-                    buttonCounting2.setClickable(false);
-                    buttonGeo.setVisibility(View.INVISIBLE);
-                    buttonGeo.setClickable(false);
-                    buttonSummation1.setVisibility(View.INVISIBLE);
-                    buttonSummation1.setClickable(false);
-                    buttonSubtraction1.setVisibility(View.INVISIBLE);
-                    buttonSubtraction1.setClickable(false);
-                    buttonSCounting.setVisibility(View.VISIBLE);
-                    buttonSCounting.setClickable(true);
-                    buttonSGeo.setVisibility(View.VISIBLE);
-                    buttonSGeo.setClickable(true);
+                    setStudy();
                 }
                 else if (position == 1){
-                    buttonCounting.setVisibility(View.VISIBLE);
-                    buttonCounting.setClickable(true);
-                    buttonCounting2.setVisibility(View.VISIBLE);
-                    buttonCounting2.setClickable(true);
-                    buttonGeo.setVisibility(View.VISIBLE);
-                    buttonGeo.setClickable(true);
-                    buttonSummation1.setVisibility(View.VISIBLE);
-                    buttonSummation1.setClickable(true);
-                    buttonSubtraction1.setVisibility(View.VISIBLE);
-                    buttonSubtraction1.setClickable(true);
-                    buttonSCounting.setVisibility(View.INVISIBLE);
-                    buttonSCounting.setClickable(false);
-                    buttonSGeo.setVisibility(View.INVISIBLE);
-                    buttonSGeo.setClickable(false);
+                    setTest();
                 }
             }
 
