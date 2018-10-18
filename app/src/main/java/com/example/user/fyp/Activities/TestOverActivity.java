7c64f6ed0e2b53c33469cc37d9660fb3261ee6ab
Devanw <devanewaldo@gmail.com>
2018-10-18 22:21:37 +0800

    Set Score Text

--- app/src/main/java/com/example/user/fyp/Activities/TestOverActivity.java
+++ app/src/main/java/com/example/user/fyp/Activities/TestOverActivity.java
@@ -3,15 +3,27 @@ package com.example.user.fyp.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.user.fyp.R;
 
+import org.w3c.dom.Text;
+
 public class TestOverActivity extends AppCompatActivity {
+
+    TextView highScore;
+
+    protected void establish(){
+        highScore = findViewById(R.id.testOver_score);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_over);
+        establish();
+        String intentScore = getIntent().getExtras().getString("HighScore");
+        highScore.setText(intentScore);
     }
 
     @Override
