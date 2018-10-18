dc9aa07ffa01b64e330b92e0818f14ea07c832d8
Devanw <devanewaldo@gmail.com>
2018-10-19 01:32:12 +0800

    Added Highscore for each challenge Implemented SharedPreference for Spinner

--- app/src/main/java/com/example/user/fyp/Activities/MathTopicActivity.java
+++ app/src/main/java/com/example/user/fyp/Activities/MathTopicActivity.java
@@ -1,14 +1,20 @@
 package com.example.user.fyp.Activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.user.fyp.Math.CountingTopic;
 import com.example.user.fyp.Math.CountingTopic2;
@@ -22,6 +28,13 @@ import com.example.user.fyp.R;
 public class MathTopicActivity extends AppCompatActivity {
 
     private static final String TAG = "MathTopic";
+    public static final String MATHTOPIC_SPINNER_KEY = "MATHTOPIC_SPINNER_KEY";
+
+    CountingTopic count1;
+    CountingTopic2 count2;
+    Summation1 summ1;
+    Subtraction1 subt1;
+    Geometry1 geom1;
 
     Button buttonCounting;
     Button buttonCounting2;
@@ -32,6 +45,15 @@ public class MathTopicActivity extends AppCompatActivity {
     Button buttonSGeo;
     Spinner spinnerStudyOrChallenge;
     ArrayAdapter<CharSequence> adapter;
+
+    TextView highScoreTag;
+    TextView highScore1;
+    TextView highScore2;
+    TextView highScore3;
+    TextView highScore4;
+    TextView highScore5;
+
+    SharedPreferences sharedPreferences;
 
     protected void establish(){
         buttonCounting = findViewById(R.id.mathTopicActivity_bCounting1);
@@ -45,6 +67,21 @@ public class MathTopicActivity extends AppCompatActivity {
         adapter = ArrayAdapter.createFromResource(this, R.array.spinnerFill, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerStudyOrChallenge.setAdapter(adapter);
+
+        try {
+            sharedPreferences = getSharedPreferences("MySharedPreMain", Context.MODE_PRIVATE);
+            spinnerStudyOrChallenge.setSelection(sharedPreferences.getInt(MATHTOPIC_SPINNER_KEY, 0));
+            Log.d(TAG, "establish: gamauuuuuu");
+        }
+        catch (Exception e){
+            Log.d(TAG, "establish: gamau");
+        }
+        highScoreTag = findViewById(R.id.mathTopicActivity_tvHighScore1);
+        highScore1 = findViewById(R.id.mathTopicActivity_tvHighScore2);
+        highScore2 = findViewById(R.id.mathTopicActivity_tvHighScore3);
+        highScore3 = findViewById(R.id.mathTopicActivity_tvHighScore4);
+        highScore4 = findViewById(R.id.mathTopicActivity_tvHighScore5);
+        highScore5 = findViewById(R.id.mathTopicActivity_tvHighScore6);
     }
 
     protected void setChallenge(){
@@ -62,6 +99,13 @@ public class MathTopicActivity extends AppCompatActivity {
         buttonSCounting.setClickable(false);
         buttonSGeo.setVisibility(View.INVISIBLE);
         buttonSGeo.setClickable(false);
+
+        highScoreTag.setVisibility(View.VISIBLE);
+        highScore1.setVisibility(View.VISIBLE);
+        highScore2.setVisibility(View.VISIBLE);
+        highScore3.setVisibility(View.VISIBLE);
+        highScore4.setVisibility(View.VISIBLE);
+        highScore5.setVisibility(View.VISIBLE);
     }
 
     protected void setStudy(){
@@ -79,6 +123,13 @@ public class MathTopicActivity extends AppCompatActivity {
         buttonSCounting.setClickable(true);
         buttonSGeo.setVisibility(View.VISIBLE);
         buttonSGeo.setClickable(true);
+
+        highScoreTag.setVisibility(View.INVISIBLE);
+        highScore1.setVisibility(View.INVISIBLE);
+        highScore2.setVisibility(View.INVISIBLE);
+        highScore3.setVisibility(View.INVISIBLE);
+        highScore4.setVisibility(View.INVISIBLE);
+        highScore5.setVisibility(View.INVISIBLE);
     }
 
     @Override
@@ -88,12 +139,46 @@ public class MathTopicActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    protected void setSpinner0(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(MATHTOPIC_SPINNER_KEY, 0);
+        editor.commit();
+        Log.d(TAG, "setSpinner0: gamau");
+    }
+
+    protected void setSpinner1(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(MATHTOPIC_SPINNER_KEY, 1);
+        editor.commit();
+        Log.d(TAG, "setSpinner1: gamau");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_topic);
 
         establish();
+
+        sharedPreferences = getSharedPreferences("MySharedPreMain", Context.MODE_PRIVATE);
+        if (sharedPreferences.contains(count1.COUNTING11_HIGHSCORE_KEY)){
+            highScore1.setText(Integer.toString(sharedPreferences.getInt(count1.COUNTING11_HIGHSCORE_KEY,0)));
+        }
+        if (sharedPreferences.contains(count2.COUNTING2_HIGHSCORE_KEY)){
+            highScore2.setText(Integer.toString(sharedPreferences.getInt(count2.COUNTING2_HIGHSCORE_KEY,0)));
+        }
+        if (sharedPreferences.contains(summ1.SUMMATION1_HIGHSCORE_KEY)){
+            highScore3.setText(Integer.toString(sharedPreferences.getInt(summ1.SUMMATION1_HIGHSCORE_KEY,0)));
+        }
+        if (sharedPreferences.contains(subt1.SUBTRACTION1_HIGHSCORE_KEY)){
+            highScore4.setText(Integer.toString(sharedPreferences.getInt(subt1.SUBTRACTION1_HIGHSCORE_KEY,0)));
+        }
+        if (sharedPreferences.contains(geom1.GEOMETRY1_HIGHSCORE_KEY)){
+            highScore5.setText(Integer.toString(sharedPreferences.getInt(geom1.GEOMETRY1_HIGHSCORE_KEY,0)));
+        }
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String value = preferences.getString("", "defaultValue");
 
         buttonSummation1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -170,9 +255,11 @@ public class MathTopicActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if (position == 0){
+                    setSpinner0();
                     setStudy();
                 }
                 else if (position == 1){
+                    setSpinner1();
                     setChallenge();
                 }
             }
